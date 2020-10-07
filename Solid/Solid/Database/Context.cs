03346d161d3bae607e40d6168a83a431875a5b0b
Bruno Augusto de Souza Oliveira <brunoaugustoliveira@gmail.com>
2020-10-07 15:20:44 -0300

    Criada classe de contexto para conexão com o banco de dados

---
+++ Solid/Solid/Database/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Solid.Database
+{
+    public class Context : DbContext
+    {
+        public Context() { }
+
+        public Context(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\SOLID");
+        }
+    }
+}
