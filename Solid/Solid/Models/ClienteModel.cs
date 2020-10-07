cc02b6ef951d81723d35718da3e986d047d0e23c
Bruno Augusto de Souza Oliveira <brunoaugustoliveira@gmail.com>
2020-10-07 15:33:59 -0300

    Criada classe modelo cliente

---
+++ Solid/Solid/Models/ClienteModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solid.Models
+{
+    [Table("CLIENTE")]
+    public class ClienteModel
+    {
+        [Key, Column("ID_CLIENTE")]
+        public int IdCliente { get; set; }
+
+        [Column("NOME")]
+        public string Nome { get; set; }
+
+        [Column("CPF_CNPJ")]
+        public string CpfCnpj { get; set; }
+
+        [Column("DATA_NASCIMENTO")]
+        public DateTime? DataNascimento { get; set; }
+    }
+}
